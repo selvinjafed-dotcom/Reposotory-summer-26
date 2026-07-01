86433a7d9699cffc9dd953909467ebf937d6f3e3
selvinjafed-dotcom <selvinjafed@gmail.com>
2026-06-30 21:45:10 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,44 @@
-let imgumbrella;
-
-let imgbeachball;
+let imgUmbrella;
+let imgBeachBall;
 
 function preload() {
-
-imgumbrella=loadImage('images/umbrella.png');
-imgbeachball =loadImage('images/beachball.png');
-
-
-
+    imgUmbrella = loadImage('images/umbrella.png');
+    imgBeachBall = loadImage('images/beachball.png');
 }
 
-
-function setup () {
-
-createCanvas (400,400);
-
+function setup() {
+    createCanvas(800, 600); 
 }
 
 function draw() {
- background(135,206,235);
- 
-//sun
- fill(255, 240, 0);
- noStroke();
-ellipse(320, 80, 60, 60);
-
-
-//cloud
-fill(255);
-noStroke();
-rect(190, 80, 100, 60, 20);
-
-//sea
-fill(0, 105, 148);
-noStroke();
-rect(0,200, 400, 100);
-
-//sand
-fill(194, 178, 128);
-noStroke();
-rect(0, 300, 400, 100);
-
-
-//Tree
-fill(139, 69, 19);
-rect(52, 240, 15, 70);
-
-fill(34, 139, 34);
-noStroke();
-ellipse(60, 240, 50, 60);
-
-image(imgumbrella, 230, 210, 120, 120);
-image(imgbeachball, 200, 310, 35, 35);
-image(imgbeachball, 110, 320, 45, 45);
-image(imgbeachball, 20, 340, 55, 55);
-
-
+    background(135, 206, 235);
+    
+    fill(255, 240, 0);
+    noStroke();
+    ellipse(650, 120, 120, 120);
+    
+    fill(255);
+    noStroke();
+    rect(350, 100, 250, 50, 40);
+    
+    fill(0, 105, 148);
+    noStroke();
+    rect(0, 280, 800, 140);
+    
+    fill(194, 178, 128);
+    noStroke();
+    rect(0, 420, 800, 180);
+    
+    fill(139, 69, 19);
+    rect(120, 320, 30, 140); 
+    
+    fill(34, 139, 34);
+    noStroke();
+    ellipse(135, 320, 120, 120); 
+    
+    image(imgUmbrella, 450, 260, 240, 240);
+    
+    image(imgBeachBall, 400, 460, 70, 70);
+    image(imgBeachBall, 250, 490, 85, 85);
+    image(imgBeachBall, 50, 480, 100, 100);
 }
