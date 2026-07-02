49edc47b3fc8903e8f9402f253be30baec542f87
selvinjafed-dotcom <selvinjafed@gmail.com>
2026-07-01 22:09:27 -0500

    Add files via upload

--- interactivecolorandshape.js
+++ interactivecolorandshape.js
@@ -1,88 +1,73 @@
-let lineBrushtool = false;
-let ellipseBrushtool = false;
-let showBeachBall = false;
+let imgUmbrella;
 let imgBeachBall;
 
+let showUmbrella = false;
+let showBeachBall = false;
+
 function preload() {
-  imgBeachBall = loadImage('images/beachball.png');
+    imgUmbrella = loadImage('images/umbrella.png');
+    imgBeachBall = loadImage('images/beachball.png');
 }
 
-function setup() { 
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+function setup() {
+    createCanvas(800, 600); 
 }
 
 function draw() {
-  push();
-  noStroke();
-  fill(255);
-  rect(15, 15, 180, 130);
-  fill(0);
-  textSize(16);
-  textAlign(LEFT, TOP);
-  text("Controls:\nL - Line Brush\nE - Ellipse Brush\nB - Ball\nO - Off\nC - Clear\nS - Save", 20, 20);
-  pop();
+    background(135, 206, 235);
+    
+    fill(255, 240, 0);
+    noStroke();
+    ellipse(650, 120, 120, 120);
+    
+    fill(255);
+    noStroke();
+    rect(350, 100, 250, 50, 40);
+    
+    fill(0, 105, 148);
+    noStroke();
+    rect(0, 280, 800, 140);
+    
+    fill(194, 178, 128);
+    noStroke();
+    rect(0, 420, 800, 180);
+    
+    fill(139, 69, 19);
+    rect(120, 320, 30, 140); 
+    
+    fill(34, 139, 34);
+    noStroke();
+    ellipse(135, 320, 120, 120); 
+    
+    if (showUmbrella === true) {
+        image(imgUmbrella, 450, 260, 240, 240);
+    }
+    
+    if (showBeachBall === true) {
+        image(imgBeachBall, 400, 460, 70, 70);
+        image(imgBeachBall, 250, 490, 85, 85);
+        image(imgBeachBall, 50, 480, 100, 100);
+    }
 
-  if (showBeachBall && imgBeachBall) {
-    image(imgBeachBall, 400, 460, 70, 70);
-  }
-
-  if (lineBrushtool == true) {
-    lineBrush();
-  }
-
-  if (ellipseBrushtool == true) {
-    ellipseBrush();
-  }
-}
-
-function lineBrush() {
-  stroke(random(255), random(255), random(255));
-  strokeWeight(15);
-
-  if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
-}
-
-function ellipseBrush() {
-  noStroke(); 
-  if (mouseIsPressed) {
-    fill(random(255), random(255), random(255));
-    ellipse(mouseX, mouseY, 20, 20);
-  }
+    fill(0);
+    textSize(16);
+    textAlign(LEFT, TOP);
+    text("press J for beach ball", 20, 20);
+    text("press D for umbrella", 20, 45);
+    text("press R to reset", 20, 70);
 }
 
 function keyPressed() {
-  if (key === 'l' || key === 'L') {
-    lineBrushtool = true;
-    ellipseBrushtool = false;
-  }
-
-  if (key === 'e' || key === 'E') {
-    lineBrushtool = false;
-    ellipseBrushtool = true;
-  }
-
-  if (key === 'b' || key === 'B') {
-    showBeachBall = true;
-  }
-
-  if (key === 'o' || key === 'O') {
-    lineBrushtool = false;
-    ellipseBrushtool = false;
-  }
-
-  if (key === 'c' || key === 'C') {
-    lineBrushtool = false;
-    ellipseBrushtool = false;
-    showBeachBall = false;
-    background(255);
-  }
-
-  if (key === 's' || key === 'S') {
-    lineBrushtool = false;
-    ellipseBrushtool = false;
-    save('mydrawing.jpg');
-  }
+    if (key === 'D' || key === 'd') {
+        showUmbrella = true;
+    }
+    
+    if (key === 'J' || key === 'j') {
+        showBeachBall = true;
+    }
+    
+    if (key === 'R' || key === 'r') {
+        showUmbrella = false;
+        showBeachBall = false;
+    }
 }
